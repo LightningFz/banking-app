99d5385b95349f5cb7b194f4fdafdef4fe0208a2
LightningFz <katfawaz64@gmail.com>
2024-08-24 22:51:00 -0400

    Header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import HeaderBox from '@/components/ui/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Fawaz'}
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your account and transactions effeiently.'
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
