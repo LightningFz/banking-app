e140ea6b3ca578458e07c74378bca8172339dbcd
LightningFz <katfawaz64@gmail.com>
2024-09-02 20:41:59 -0400

    appwrite sign up

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,10 @@
 'use server'
 
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { cookies } from "next/headers";
+import { parseStringify } from "../utils";
+
 export const signIn = async () => {
     try{
         //Mutation / Database / Make fetch 
@@ -8,9 +13,36 @@ export const signIn = async () => {
     }
 }
 export const signUp = async (userData: SignUpParams) => {
+
     try{
-        //creat user acc
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(
+            ID.unique(), 
+            userData.email, 
+            userData.password, 
+            '${firstName} {lastName}'
+        );
+
+        const session = await account.createEmailPasswordSession(email, password);
+
+        cookies().set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        })
+      return parseStringify(newUserAccount);
     } catch (error) {
         console.error('Error', error);
     }
 }
+
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      return await account.get();
+    } catch (error) {
+      return null;
+    }
+  }
