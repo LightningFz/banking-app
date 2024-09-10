b3d05f6c15d7e7b11436f09d01ced681fccc3d71
LightningFz <katfawaz64@gmail.com>
2024-09-09 23:01:52 -0400

    plaid

---
+++ lib/plaid.ts
@@ -0,0 +1,12 @@
+import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+    }
+  }
+})
+
+export const plaidClient = new PlaidApi(configuration);
