9e3e368c73dfd31dfdeeb9661acd5c930137dc23
LightningFz <katfawaz64@gmail.com>
2024-09-11 22:11:07 -0400

    plaid backend

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -22,6 +22,7 @@ import { authFormSchema } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions'
+import PlaidLink from './PlaidLink'
 
 
 const AuthForm = ({ type }: {type: string}) => {
@@ -96,11 +97,11 @@ const AuthForm = ({ type }: {type: string}) => {
                 </h1>
             </div>
         </header>
-        {user ? (
+        {/*{user ? (*/}
             <div className='flex flex-col gap-4'>
-                {/*Plaidlink */}
+                <PlaidLink user={user} variant="primary" />
             </div>
-            ): (
+        {/*}): (*/}
                <>
                    <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -152,8 +153,7 @@ const AuthForm = ({ type }: {type: string}) => {
                     </Link>
                 </footer>
                </> 
-            )
-        }
+        {/*})}*/}
     </section>
   )
 }
