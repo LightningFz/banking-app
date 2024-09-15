b82cce300495d522c7c9508170e8f497689f3a3f
LightningFz <katfawaz64@gmail.com>
2024-09-14 23:07:57 -0400

    dwolla bug fixed

--- components/ui/AuthForm.tsx
+++ components/ui/AuthForm.tsx
@@ -45,10 +45,24 @@ const AuthForm = ({ type }: {type: string}) => {
         const onSubmit = async (data: z.infer<typeof formSchema>) => {
             setIsLoading(true);
             try{
-                // Sign up appwrite & create plaid token
 
+                // Sign up appwrite & create plaid token
                 if(type === 'sign-up'){
-                  const newUser = await signUp(data);
+                    if(type === 'sign-up') {
+                        const userData = {
+                          firstName: data.firstName!,
+                          lastName: data.lastName!,
+                          address1: data.address1!,
+                          city: data.city!,
+                          state: data.state!,
+                          postalCode: data.postalCode!,
+                          dateOfBirth: data.DOB!,
+                          ssn: data.ssn!,
+                          email: data.email,
+                          password: data.password
+                        }
+                
+                  const newUser = await signUp(userData);
 
                   setUser(newUser);
                 }
